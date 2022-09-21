0a780b45636f3677d0302e44b3fad38de0215f85
Italo Ferreira <grd788@gmail.com>
2022-09-20 23:51:10 -0500

    feat :sparkles: component to duo card and duo iinfo

--- mobile/src/components/GameCard/index.tsx
+++ mobile/src/components/GameCard/index.tsx
@@ -15,9 +15,11 @@ import { THEME } from '../../theme'
 
 export interface GameCardProps {
   id: string
-  name: string
-  ads: string
-  cover: ImageSourcePropType
+  title: string
+  bannerUrl: string
+  _count: {
+    ads: number
+  }
 }
 
 interface Props extends TouchableOpacityProps {
@@ -27,10 +29,10 @@ interface Props extends TouchableOpacityProps {
 export function GameCard({ data, ...rest }: Props) {
   return (
     <TouchableOpacity style={styles.container} {...rest}>
-      <ImageBackground style={styles.cover} source={data.cover}>
+      <ImageBackground style={styles.cover} source={{ uri: data.bannerUrl }}>
         <LinearGradient colors={THEME.COLORS.FOOTER} style={styles.footer}>
-          <Text style={styles.name}>{data.name}</Text>
-          <Text style={styles.ads}>{data.ads} Anuncios</Text>
+          <Text style={styles.name}>{data.title}</Text>
+          <Text style={styles.ads}>{data._count.ads} Anuncios</Text>
         </LinearGradient>
       </ImageBackground>
     </TouchableOpacity>
